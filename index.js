95a7a61a6a7979fabcbc2adf31ff536b35e12de9
ABukhtenko <abukhtenko@gmail.com>
2025-01-26 23:27:45 +0200

    comment added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function sayHello(name, surname) {
   console.log(`Morning ${name} ${surname}`);
   //New feature is no needed anymore
+  //rebase again from feature-2
 }
 
 sayHello("Robots", "Are dreaming");
