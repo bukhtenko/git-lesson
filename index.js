560f14e062fa7b99e94020736214b49452a01f07
ABukhtenko <abukhtenko@gmail.com>
2025-01-27 16:41:20 +0200

    code by Martin

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function sayHello(name, surname) {
   console.log(`Morning ${name} ${surname}`);
-  //New feature is no needed anymore
-  //rebase again from feature-2
 }
 
 sayHello("Robots", "Are dreaming");
-//strange comment
